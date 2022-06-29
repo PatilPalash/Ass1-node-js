30f38b4768a20a8d08d360d408923abf3b39f344
Palash Patil <97968344+PatilPalash@users.noreply.github.com>
2022-06-29 06:54:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require("http");
 const server = http.createServer(function(require,response){
-    response.write("<h1>this i nodes js class</h1>")
+    
+response.write("This is my First Server")
+    
+response.write("<h1> This is my First Server</h1>")
+    
+response.write(JSON.stringify({Name:"This is my First Server",Age:25}))
+    
 response.end();
 })
 server.listen(4000)
